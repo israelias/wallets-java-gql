96d27373aada741c1bfc8a435aa9f285e85ef1a7
israelias <israelias.js@gmail.com>
2021-09-30 22:07:41 +0800

    Consider option that returns partial data
    Option returns the data fetcher result along with whatever data and error the query fetched

---
+++ src/main/java/com/example/springbootgraphql/exceptions/CustomGraphqlErrorHandler.java
@@ -0,0 +1,19 @@
+package com.example.springbootgraphql.exceptions;
+
+import graphql.GraphQLError;
+import graphql.kickstart.execution.error.GraphQLErrorHandler;
+import java.util.List;
+import org.springframework.stereotype.Component;
+
+// Disguise the exception messages if we're not happy with it
+// or type of error set back
+// or send the whole thing back
+
+@Component
+public class CustomGraphqlErrorHandler implements GraphQLErrorHandler {
+
+    @Override
+    public List<GraphQLError> processErrors(List<GraphQLError> errors) {
+        return errors;
+    }
+}
--- src/main/java/com/example/springbootgraphql/resolver/bank/ClientResolver.java
+++ src/main/java/com/example/springbootgraphql/resolver/bank/ClientResolver.java
@@ -3,6 +3,8 @@ package com.example.springbootgraphql.resolver.bank;
 import com.example.springbootgraphql.domain.bank.BankAccount;
 import com.example.springbootgraphql.domain.bank.Client;
 import graphql.GraphQLException;
+import graphql.execution.DataFetcherResult;
+import graphql.kickstart.execution.error.GenericGraphQLError;
 import graphql.kickstart.tools.GraphQLResolver;
 import java.util.UUID;
 import lombok.extern.slf4j.Slf4j;
@@ -12,11 +14,16 @@ import org.springframework.stereotype.Component;
 @Component
 public class ClientResolver implements GraphQLResolver<BankAccount> {
 
-    public Client client(BankAccount bankAccount) {
+    public DataFetcherResult<Client> client(BankAccount bankAccount) {
         log.info("Requesting client data for bank account id {}", bankAccount.getId());
 
-        throw new GraphQLException("Client unavailable");
+
+        //        throw new GraphQLException("Client unavailable");
+        // throw new RuntimeException("Spring exception can't connect to database: (sql select *)");
         //        return
-        // Client.builder().id(UUID.randomUUID()).firstName("Elias").lastName("Wrubel").build();
+         return DataFetcherResult.<Client>newResult()
+             .data(Client.builder().id(UUID.randomUUID()).firstName("Elias").lastName("Wrubel").build())
+             .error(new GenericGraphQLError("Could not get sub-client id"))
+             .build();
     }
 }
