278e626b453e4da116f7f620a235dc7b6185e300
israelias <israelias.js@gmail.com>
2021-10-06 02:19:03 +0800

    Write correlation id executor wrapper

--- src/main/java/com/example/springbootgraphql/util/CorrelationIdPropagationExecutor.java
+++ src/main/java/com/example/springbootgraphql/util/CorrelationIdPropagationExecutor.java
@@ -1,2 +1,48 @@
-package com.example.springbootgraphql.util;public class CorrelationIdPropagationExecutor {
+package com.example.springbootgraphql.util;
+
+import static com.example.springbootgraphql.instrumentation.RequestLoggingInstrumentation.CORRELATION_ID;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.RejectedExecutionException;
+import lombok.RequiredArgsConstructor;
+import org.jetbrains.annotations.NotNull;
+import org.slf4j.MDC;
+
+@RequiredArgsConstructor
+public class CorrelationIdPropagationExecutor implements Executor {
+
+  // delegate main executor
+  private final Executor delegate;
+
+  public static Executor wrap(Executor executor) {
+    return new CorrelationIdPropagationExecutor(executor);
+  }
+
+  /**
+   * Executes the given command at some time in the future. The command may execute in a new thread,
+   * in a pooled thread, or in the calling thread, at the discretion of the {@code Executor}
+   * implementation.
+   *
+   * <p>Used to propagate the `correlation_id` to the executor
+   *
+   * @param command the runnable task
+   * @throws RejectedExecutionException if this task cannot be accepted for execution
+   * @throws NullPointerException if command is null
+   */
+  @Override
+  public void execute(@NotNull Runnable command) {
+    var correlationId = MDC.get(CORRELATION_ID);
+
+    delegate.execute(
+        () -> {
+          try {
+            MDC.put(CORRELATION_ID, correlationId);
+            command.run();
+
+          } finally {
+            MDC.remove(CORRELATION_ID);
+          }
+        });
+
+  }
 }
